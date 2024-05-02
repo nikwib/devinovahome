ef4972f040be7ed835c6b1039ad3cf27f918cf62
SaraPellnor <sara.ahlquist@hotmail.com>
2024-05-02 11:45:13 +0200

    otions list in section data

--- schemaTypes/sectionData.ts
+++ schemaTypes/sectionData.ts
@@ -6,53 +6,78 @@ export default {
     {
       name: 'type',
       type: 'string',
-      title: 'Type'
+      title: 'Type',
+      options: {
+        list: [
+          {title: 'Hero', value: 'HeroSection'},
+          {title: 'Text Image 1', value: 'TextImage1'},
+          {title: 'Text Image 2', value: 'TextImage2'},
+        ],
+      },
     },
     {
       name: 'head',
       type: 'string',
-      title: 'Head'
+      title: 'Head',
     },
     {
       name: 'subHeadAbove',
       type: 'string',
-      title: 'SubHeadAbove'
+      title: 'SubHead Above',
     },
     {
       name: 'subHead',
       type: 'string',
-      title: 'SubHead'
-    },    {
+      title: 'SubHead',
+    },
+    {
       name: 'title',
       type: 'string',
-      title: 'Title'
+      title: 'Title',
     },
     {
       name: 'body',
       type: 'string',
-      title: 'Body'
-    },    {
+      title: 'Body',
+    },
+    {
       name: 'image',
       type: 'string',
-      title: 'Image'
-    },    {
+      title: 'Image',
+    },
+    {
       name: 'buttonText',
       type: 'string',
-      title: 'ButtonText'
-    },    {
+      title: 'Button Text',
+    },
+    {
       name: 'buttonType',
       type: 'string',
-      title: 'ButtonType'
-    },    {
+      title: 'Button Type',
+      options: {
+        list: [
+          {title: 'Arrow Button', value: 'ArrowBtn'},
+          {title: 'Arrow Inside Circle Button', value: 'ArrowInCircleBtn'},
+          {title: 'Open New Window Button', value: 'OpenNewWindowBtn'},
+        ],
+      },
+    },
+    {
       name: 'buttonIcon',
       type: 'string',
-      title: 'ButtonIcon'
-    },  
+      title: 'Button Icon',
+      options: {
+        list: [
+          {title: 'Arrow', value: 'FaLongArrowAltRight'},
+          {title: 'Arrow Inside Circle', value: 'FaArrowAltCircleRight'},
+          {title: 'Open', value: 'RxOpenInNewWindow'},
+        ],
+      },
+    },
     {
       name: 'action',
       type: 'string',
-      title: 'Action'
+      title: 'Action',
     },
-    
-  ]
+  ],
 }
